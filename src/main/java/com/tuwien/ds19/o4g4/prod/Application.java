b3ae82818e65f6cf06b69b70d7971b2db693457d
Lukas Inschlag <inschi123@gmail.com>
2019-06-22 13:55:18 +0200

    Initial commit

---
+++ src/main/java/com/tuwien/ds19/o4g4/prod/Application.java
@@ -0,0 +1,13 @@
+package com.tuwien.ds19.o4g4.prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
